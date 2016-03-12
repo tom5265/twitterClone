2a323dbcbde8fd59bdecc48537c67928a825224b
Tom Moore <tom5265@gmail.com>
2016-03-12 15:53:23 -0600

    added styling, prelim structure

--- client/js/main.js
+++ client/js/main.js
@@ -4,10 +4,29 @@ $(document).ready(function(){
 		/*This function should create a post request using jquery. When posted it should:
 			1) Add tweets to the 'database'
 			2) After posted prepend message to list of messages and clear input box */
+            // $.ajax({
+            //     method: 'POST',
+            //     url: 'messages.txt',
+            //     success: function() {
+            //         var text = $(input).val();
+            //         $('#tweet_list').prepend(<li>text</li>)
+            //     }
+                
+            // })
+            
 	}
 
 	function getData() {
 		/*This function should make a get request from 'database', parse the data and prepend each to the page*/
+        $.ajax({
+                method: 'GET',
+                url: 'messages.txt',
+                success: function(tweets) {
+                    $.each(tweets, function (i, tweet) {
+                        $('#tweet_list').prepend(<li>tweet</li>)
+                    })
+                }
+            })
 	}
 
 	/*Calls function once page loaded to display tweets to page*/
