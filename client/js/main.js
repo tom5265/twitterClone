c62eaff1d2381602ee0311ade89fd6a9b5380736
Tom Moore <tom5265@gmail.com>
2016-03-13 21:52:48 -0500

    tweets show up, working on post

--- client/js/main.js
+++ client/js/main.js
@@ -1,36 +1,53 @@
-$(document).ready(function(){
-/*Using document ready runs code only after the DOM is ready for js code to run more on that here: https://learn.jquery.com/using-jquery-core/document-ready */
-	function postData() {
+$(document).ready(function() {
+    $('#btn').click(function () {
+        var text = $('#inp').val();
+        var anonymous = 'anonymous';
+        postData(text, anonymous)
+    })
+    
+
+
+
+
+    /*Using document ready runs code only after the DOM is ready for js code to run more on that here: https://learn.jquery.com/using-jquery-core/document-ready */
+    function postData(tweet, user) {
 		/*This function should create a post request using jquery. When posted it should:
 			1) Add tweets to the 'database'
 			2) After posted prepend message to list of messages and clear input box */
-            $.ajax({
-                method: 'POST',
-                url: 'messages.txt',
-                data:      
-            }).then(function() {
-                    var text = $('#inp').val();
-                    $('#tweet_list').prepend('<li>text</li>')
-                })
-            
-	}
-
-	function getData() {
-		/*This function should make a get request from 'database', parse the data and prepend each to the page*/
+        var msg = {};
+        msg.text = tweet;
+        msg.userName = user;
         $.ajax({
-                method: 'GET',
-                url: 'messages.txt',
-                success: function(tweets) {
-                    $.each(tweets, function (i, tweet) {
-                        $('#tweet_list').prepend('<li>tweet</li>')
-                    })
-                }
-            })
-	}
+            method: 'POST',
+            url: 'messages',
+            data: JSON.stringify(msg)
+        }).done(function() {
+            
+        })
 
-	/*Calls function once page loaded to display tweets to page*/
-	getData();
-    $('#btn').click(function () {
-        postData();
-    })
+    }
+
+    function getData() {
+        /*This function should make a get request from 'database', parse the data and prepend each to the page*/
+        $.ajax({
+            method: 'GET',
+            url: 'messages',
+        }).done(function(data) {
+
+            var tweets = data.split('\n');
+
+            for (var i = 0; i < tweets.length; i++) {
+                var tweet = JSON.parse(tweets[i]);
+                var twit = tweet.text;
+
+                $('#tweet_list').prepend('<li>' + twit + '</li>');
+            }
+        })
+    }
+
+    /*Calls function once page loaded to display tweets to page*/
+    getData();
+
 });
+
+// var text = $('#inp').val();
